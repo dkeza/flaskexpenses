a2c4d02f4c8e36fe89a26b30d1844937097a5f4a
keza <dkezic@yahoo.com>
2018-02-18 12:26:40 +0100

    Add Bootstrap. Add config.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return '<h1>Landed</h1>'
+    return render_template('index.html', title='Home')
