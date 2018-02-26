bef0844811b1b18421647e11f10c9ac0ca96ed8c
keza <dkezic@yahoo.com>
2018-02-26 19:26:38 +0100

    Get mail admin setting to enviroment variable. Add migrate module. Add password_hash field to user table.

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['office@citadel.rs']
+    ADMINS = [os.environ.get('MAIL_ADMIN')]
     POSTS_PER_PAGE = 3
